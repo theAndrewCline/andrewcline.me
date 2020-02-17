d8d0534f56993c1bf3f6d4ae0200662837d02f15
Andrew Cline <andrew.cline77@gmail.com>
2020-02-16 18:56:24 -0600

    update style

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, h1, h3, img, text)
-import Html.Attributes exposing (class, src)
+import Html exposing (Html, a, button, div, h1, h3, img, li, text, ul)
+import Html.Attributes exposing (class, href, src)
 
 
 
@@ -38,10 +38,23 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div [ class "min-h-screen flex flex-col align-center bg-gray-100" ]
-        [ h1 [ class "cursive" ] [ text "Andrew Cline" ]
-        , img [ src "./headshot.jpg", class "mx-auto rounded-full" ] []
-        , h3 [ class "font-serif" ] [ text "Software Developer" ]
-        , h3 [ class "font-serif" ] [ text "Peoria, IL" ]
+        [ ul [ class "flex bg-black py-4 font-body font-bold" ]
+            [ li [ class "mx-4 text-gray-100 bold mr-auto font-display" ] [ text "Andrew Cline" ]
+
+            -- , li [ class "mx-4 text-gray-100" ] [ text "Bio" ]
+            -- , li [ class "mx-4 text-gray-100" ] [ text "Resume" ]
+            -- , li [ class "mx-4 text-gray-100" ] [ text "Projects" ]
+            ]
+        , div
+            [ class "container mx-auto my-4 py-4" ]
+            [ h1 [ class "font-display text-4xl" ] [ text "Andrew Cline" ]
+            , img [ src "./headshot.jpg", class "mx-auto rounded" ] []
+            , h3 [ class "font-body font-bold text-xl" ] [ text "Software Developer" ]
+            , h3 [ class "font-body text-lg" ] [ text "Peoria, IL" ]
+            , button [ class "bg-black text-gray-100 p-2" ]
+                [ a [ class "font-body underline", href "https://github.com/theAndrewCline" ] [ text "Github" ]
+                ]
+            ]
         ]
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      display: ['Rock Salt', 'sans-serif'],
+      body: ['PT Serif', 'serif']
+    },
     extend: {},
   },
   variants: {},
