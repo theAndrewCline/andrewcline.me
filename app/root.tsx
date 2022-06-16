07e088f99a2d664ede67c6412c1b493f274ad033
Andrew Cline <acline@precisionplanting.com>
2022-06-16 15:39:54 -0500

    feat: update tab title
    resolves #48

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ import styles from './tailwind.css'
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'Andrew Cline Blog',
   viewport: 'width=device-width,initial-scale=1'
 })
 
